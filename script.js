c6158ec36d8014d0f2d959855dc2668d9c96e3e8
fabian2912 <56194394+fabian2912@users.noreply.github.com>
2023-03-09 17:08:39 +0000

    added 'new button' functionality

--- script.js
+++ script.js
@@ -8,19 +8,30 @@ const score1El = document.getElementById('score--1'); // not writing a selector,
 const current0El = document.getElementById('current--0');
 const current1El = document.getElementById('current--1');
 const diceEl = document.querySelector('.dice');
-const btnNew = document.querySelector('btn--new');
+const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
+let scores, currentScore, activePlayer, playing;
 
-// starting conditions
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add('hidden');
+const init = function() {
+    scores = [0, 0];
+    currentScore = 0;
+    activePlayer = 0;
+    playing = true;
+    
+    score0El.textContent = 0;
+    score1El.textContent = 0;
+    current0El.textContent = 0;
+    current1El.textContent = 0;
+    player0El.classList.remove('player--winner');
+    player1El.classList.remove('player--winner');
+    player0El.classList.add('player--active');
+    player1El.classList.remove('player--active');
+    diceEl.classList.add('hidden');
+};
+
+init();
 
 const switchPlayer = function() {
     currentScore = 0;
@@ -76,3 +87,5 @@ btnHold.addEventListener('click', function() {
         }
     }
 })
+
+btnNew.addEventListener('click', init);
